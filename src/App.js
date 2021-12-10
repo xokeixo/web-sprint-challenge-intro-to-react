5227e91532462b90b37ef5e8efb31a4fa98c5f2f
Markeisha Wallacce <Markeisha94@gmail.com>
2021-12-09 21:12:19 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import './App.css';
+import styled, { keyframes } from 'styled-components';
+import Character from './components/Character';
+
+const kf = keyframes`
+100% {
+  opacity: 1;
+  transform: scale(1) rotateZ(0);
+}
+`
+const StyledDetails = styled.div`
+animation: ${kf} 0.5s ease-in-out forwards;
+`
+
+const Header = styled.h1`
+color: deepskyblue;
+animation: 2s ease-in-out forwards;
+background-color: aquamarine;
+font-size: 5rem;
+display: flex;
+justify-content: center;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,19 +32,24 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(() => {
-    fetch('https://swapi.dev/api/people')
+    axios
+    .get('https://swapi.dev/api/people')
     .then(res => {
+      console.log(res.data)
       setCharacters(res.data)
     })
-    .catch(err => {
-      console.log(err)
-    })
-  })
+    .catch(err => console.log(err))
+  }, [])
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <StyledDetails className='container'>
+      <Header className='header'>Characters</Header>
+      {characters.map((char) => {
+        return (
+          <Character name={char.name} />
+      )
+      })}
+    </StyledDetails>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const StyledDiv = styled.div`
+color: gray;
+background-color: white;
+`
+
+const Names = styled.h1`
+color: aquamarine;
+background-color: deepskyblue;
+`
+
+const Characters = props => {
+
+    return (
+        <StyledDiv className='container'>
+            <Names>{props.name}</Names>
+        </StyledDiv>
+    )
+}
+
+export default Characters;
